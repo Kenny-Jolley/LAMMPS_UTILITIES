56b49bd4e93388347542c578d1ee589a7ca6d447
“Kenny <kenny.jolley@gmail.com>
2019-06-18 10:56:49 +0100

    Added script to setup custom compile settings for lammps

---
+++ lammps_carbon_reaxff_spline_src/reaxc_nonbonded.h
@@ -0,0 +1,85 @@
+/*----------------------------------------------------------------------
+  PuReMD - Purdue ReaxFF Molecular Dynamics Program
+
+  Copyright (2010) Purdue University
+  Hasan Metin Aktulga, hmaktulga@lbl.gov
+  Joseph Fogarty, jcfogart@mail.usf.edu
+  Sagar Pandit, pandit@usf.edu
+  Ananth Y Grama, ayg@cs.purdue.edu
+
+  Please cite the related publication:
+  H. M. Aktulga, J. C. Fogarty, S. A. Pandit, A. Y. Grama,
+  "Parallel Reactive Molecular Dynamics: Numerical Methods and
+  Algorithmic Techniques", Parallel Computing, in press.
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation; either version 2 of
+  the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details:
+  <http://www.gnu.org/licenses/>.
+  
+  
+  Modified to include a hard coded spline to ZBL at short range 
+  for pure carbon systems.
+  Kenny Jolley  Jan 2017
+  kenny.jolley@gmail.com
+  
+  Work in progress
+  
+  ----------------------------------------------------------------------*/
+
+#ifndef __NONBONDED_H_
+#define __NONBONDED_H_
+
+#include "reaxc_types.h"
+
+// ZBL constants  pre-compute for C-C
+//#define ZBL_ACONST  0.88534
+//#define ZBL_ABOHR   0.530
+//#define ZBL_POW     0.23
+//#define ZC_         6.0
+//#define ZBL_A  ZBL_ACONST * ABOHR / ( ZC_ ** ZBL_POW + ZC_ ** ZBL_POW )
+#define ZBL_A_CC       0.15537501107375
+
+//#define E2B4PE      2.3070796E-28
+//#define ZBL_E       1.60217733E-19
+//#define ZBL_E2 = E2B4PE / ( 1E-10_PREC * ZBL_E )
+#define ZBL_E2     14.3996520035644
+
+//#define CCONST = ZC_ * ZC_ * ZBL_E2
+#define ZBL_CCONST_CC    518.387472128319
+
+// Spline parameters for the Carbon-Carbon interaction
+// C_ - C_  (0.25 - 0.95) [kcal/mol]
+
+#define FF1CC    14.02903844437025
+#define FF2CC   -38.95839820411169
+#define FF3CC   139.8615432895962
+#define FF4CC  -286.0149594211718
+#define FF5CC   262.38971755282610
+#define FF6CC   -86.98897178124618
+#define AC_C_     0.25
+#define BC_C_     0.95
+
+
+                                      
+                                         
+
+
+void vdW_Coulomb_Energy( reax_system*, control_params*, simulation_data*,
+                         storage*, reax_list**, output_controls* );
+
+void Tabulated_vdW_Coulomb_Energy( reax_system*, control_params*,
+                                   simulation_data*, storage*,
+                                   reax_list**, output_controls* );
+
+void Compute_Polarization_Energy( reax_system*, simulation_data* );
+
+void LR_vdW_Coulomb( reax_system*, storage*, control_params*,
+                int, int, double, LR_data* );
+#endif
---
+++ lammps_setup_custom_compile.py
@@ -0,0 +1,371 @@
+#!/usr/bin/env python
+
+# This script sets up the lammps source directory ready for compiling.
+
+
+# Keyword arguments:
+# verbose = True   , prints some comments to the screen.
+# use_voro = True   , Includes the voro++ package.  This must be installed before compiling lammps
+# use_modified_reaxff = True   , Includes the modified version of user-reax  with the carbon spline
+
+
+#  Kenny Jolley, June 2019
+
+# imported modules
+import os, sys
+import shutil
+
+def lammps_setup_custom_compile(**kwargs):
+
+    # Default keyword args
+    verbose = kwargs.get('verbose', False)
+    use_voro = kwargs.get('use_voro', False)
+    use_modified_reaxff = kwargs.get('use_modified_reaxff', False)
+
+    if(verbose):
+        print("\n> Running")
+        print(" verbose  =  " + str(verbose) )
+        print(" use_voro =  " + str(use_voro) )
+        print(" use_modified_reaxff =  " + str(use_modified_reaxff) )
+
+
+    # make sure we are on the stable branch and update.
+    print("> Checkout the stable branch and update")
+    os.system("git checkout stable")
+    os.system("git pull")
+
+
+    # ensure we have clean source
+
+    # ensure all object files are deleted
+    print("> Clean all object files and purge any deprecated src files")
+    os.system("make clean-all")
+
+    if (os.path.isfile("lmp_kj_mpi")):
+        os.remove('lmp_kj_mpi')
+    if (os.path.isfile("liblammps_kj_mpi.so")):
+        os.remove('liblammps_kj_mpi.so')
+
+    # remove any deprecated src files
+    os.system("make purge")
+    # sync package files with src files
+    print("> sync package files with src files")
+    os.system("make package-update")
+
+
+    # Install packages (uncomment the packages you want included)
+    print("> Install standard packages")
+    # std packages
+    os.system("make no-std")  # Ensure we start with no std packages
+    os.system("make yes-asphere")
+    os.system("make yes-body")
+    os.system("make yes-class2")
+    os.system("make yes-colloid")
+    #os.system("make yes-compress")
+    os.system("make yes-coreshell")
+    os.system("make yes-dipole")
+    #os.system("make yes-gpu")
+    os.system("make yes-GRANULAR")
+    #os.system("make yes-KIM")
+    #os.system("make yes-KOKKOS")
+    os.system("make yes-KSPACE")
+    os.system("make yes-MANYBODY")
+    os.system("make yes-MC")
+    #os.system("make yes-MEAM")
+    os.system("make yes-MISC")
+    os.system("make yes-MOLECULE")
+    os.system("make yes-MPIIO")
+    #os.system("make yes-OPT")
+    os.system("make yes-PERI")
+    #os.system("make yes-POEMS")
+    #os.system("make yes-PYTHON")
+    os.system("make yes-QEQ")
+    #os.system("make yes-REAX")
+    os.system("make yes-REPLICA")
+    os.system("make yes-RIGID")
+    os.system("make yes-SHOCK")
+    os.system("make yes-SNAP")
+    os.system("make yes-SRD")
+    if(use_voro):
+        os.system("make yes-VORONOI")   # must install voro++ first, see instructions in lib/voronoi
+
+    # user packages (uncomment the packages you want included)
+    print("> Install user packages")
+    os.system("make no-user")  # Ensure we start with no user packages
+    os.system("make yes-USER-DIFFRACTION")
+    os.system("make yes-USER-DPD")
+    os.system("make yes-USER-DRUDE")
+    os.system("make yes-USER-EFF")
+    os.system("make yes-USER-MANIFOLD")
+    os.system("make yes-USER-MGPT")
+    os.system("make yes-USER-QTB")
+    os.system("make yes-USER-REAXC")
+    os.system("make yes-USER-SMTBQ")
+    os.system("make yes-USER-SPH")
+    os.system("make yes-USER-TALLY")
+    os.system("make yes-USER-INTEL")
+
+
+    # remove any deprecated src files
+    print("> Purge any deprecated src files")
+    os.system("make purge")
+    # sync package files with src files
+    print("> sync package files with src files")
+    os.system("make package-update")
+
+    # list available packages
+    print("> List available packages and commands for info")
+    os.system("make package")
+    # list initial package status
+    print("> List new package status")
+    os.system("make package-status")
+
+
+    # Generate the MINE directory if it does not exist
+    if (os.path.isdir("MAKE/MINE")):
+        print("> The MINE directory exists ")
+    else:
+        print("> The MINE directory does not exist, creating ")
+        os.makedirs("MAKE/MINE")
+
+    # Generate the custom makefile
+    if (os.path.isfile("MAKE/MINE/Makefile.kj_mpi")):
+        print("> Existing file MAKE/MINE/Makefile.kj_mpi removed ")
+        os.remove("MAKE/MINE/Makefile.kj_mpi")
+    # Generate file
+    print("> Generating: MAKE/MINE/Makefile.kj_mpi ")
+    outfile = open("MAKE/MINE/Makefile.kj_mpi", 'w')
+    outfile.write("""# mpi = MPI with its default compiler, mac, jpeg, png, ffmpeg, intel cpus
+SHELL = /bin/sh
+
+# ---------------------------------------------------------------------
+# compiler/linker settings
+# specify flags and libraries needed for your compiler
+
+CC =         mpicxx
+# Default
+# OPTFLAGS = -xHost -O3
+# CCFLAGS  = $(OPTFLAGS)
+#
+
+#Intel optimised
+OPTFLAGS =   -xHost -O3 -fp-model fast=2 -no-prec-div -qoverride-limits -L$(MKLROOT)/lib/ -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core
+CCFLAGS  =   -DLAMMPS_MEMALIGN=64 -fno-alias -ansi-alias -restrict -DLMP_INTEL_USELRT -DLMP_USE_MKL_RNG $(OPTFLAGS)
+
+
+SHFLAGS =    -fPIC
+DEPFLAGS =   -M
+
+LINK =       mpicxx
+
+LINKFLAGS =  $(OPTFLAGS)
+
+
+
+LIB =        -ltbbmalloc
+SIZE =       size
+
+ARCHIVE =    ar
+ARFLAGS =    -rc
+SHLIBFLAGS = -shared
+
+# ---------------------------------------------------------------------
+# LAMMPS-specific settings, all OPTIONAL
+# specify settings for LAMMPS features you will use
+# if you change any -D setting, do full re-compile after "make clean"
+
+# LAMMPS ifdef settings
+# see possible settings in Section 2.2 (step 4) of manual
+
+LMP_INC =    -DLAMMPS_GZIP -DLAMMPS_MEMALIGN=64 -DLAMMPS_JPEG -DLAMMPS_PNG -DLAMMPS_FFMPEG
+
+# MPI library
+# see discussion in Section 2.2 (step 5) of manual
+# MPI wrapper compiler/linker can provide this info
+# can point to dummy MPI library in src/STUBS as in Makefile.serial
+# use -D MPICH and OMPI settings in INC to avoid C++ lib conflicts
+# INC = path for mpi.h, MPI compiler settings
+# PATH = path for MPI library
+# LIB = name of MPI library
+
+MPI_INC =       -DMPICH_SKIP_MPICXX -DOMPI_SKIP_MPICXX=1
+MPI_PATH =
+MPI_LIB =
+
+# FFT library
+# see discussion in Section 2.2 (step 6) of manual
+# can be left blank to use provided KISS FFT library
+# INC = -DFFT setting, e.g. -DFFT_FFTW, FFT compiler settings
+# PATH = path for FFT library
+# LIB = name of FFT library
+
+FFT_INC =
+FFT_PATH =
+FFT_LIB =
+
+# JPEG and/or PNG library
+# see discussion in Section 2.2 (step 7) of manual
+# only needed if -DLAMMPS_JPEG or -DLAMMPS_PNG listed with LMP_INC
+# INC = path(s) for jpeglib.h and/or png.h
+# PATH = path(s) for JPEG library and/or PNG library
+# LIB = name(s) of JPEG library and/or PNG library
+
+JPG_INC = -I/opt/local/include
+JPG_PATH = -L/opt/local/lib
+JPG_LIB = -ljpeg -lpng
+
+# ---------------------------------------------------------------------
+# build rules and dependencies
+# do not edit this section
+
+include    Makefile.package.settings
+include    Makefile.package
+
+EXTRA_INC = $(LMP_INC) $(PKG_INC) $(MPI_INC) $(FFT_INC) $(JPG_INC) $(PKG_SYSINC)
+EXTRA_PATH = $(PKG_PATH) $(MPI_PATH) $(FFT_PATH) $(JPG_PATH) $(PKG_SYSPATH)
+EXTRA_LIB = $(PKG_LIB) $(MPI_LIB) $(FFT_LIB) $(JPG_LIB) $(PKG_SYSLIB)
+EXTRA_CPP_DEPENDS = $(PKG_CPP_DEPENDS)
+EXTRA_LINK_DEPENDS = $(PKG_LINK_DEPENDS)
+
+# Path to src files
+
+vpath %.cpp ..
+vpath %.h ..
+
+# Link target
+
+$(EXE):\t$(OBJ) $(EXTRA_LINK_DEPENDS)
+\t$(LINK) $(LINKFLAGS) $(EXTRA_PATH) $(OBJ) $(EXTRA_LIB) $(LIB) -o $(EXE)
+\t$(SIZE) $(EXE)
+
+# Library targets
+
+lib:\t$(OBJ) $(EXTRA_LINK_DEPENDS)
+\t$(ARCHIVE) $(ARFLAGS) $(EXE) $(OBJ)
+
+shlib:\t$(OBJ) $(EXTRA_LINK_DEPENDS)
+\t$(CC) $(CCFLAGS) $(SHFLAGS) $(SHLIBFLAGS) $(EXTRA_PATH) -o $(EXE) \\
+\t$(OBJ) $(EXTRA_LIB) $(LIB)
+
+# Compilation rules
+
+%.o:%.cpp
+\t$(CC) $(CCFLAGS) $(SHFLAGS) $(EXTRA_INC) -c $<
+
+# Individual dependencies
+
+depend : fastdep.exe $(SRC)
+\t@./fastdep.exe $(EXTRA_INC) -- $^ > .depend || exit 1
+
+fastdep.exe: ../DEPEND/fastdep.c
+\tcc -O -o $@ $<
+
+sinclude .depend
+""")
+
+
+    # Copy customised reax/c source files to the src directory
+    if(use_modified_reaxff):
+
+        carbon_spline_src_dir = sys.path[0] + "/lammps_carbon_reaxff_spline_src/"
+
+        
+        sys.path[0]
+        carbon_file_1 = "reaxc_nonbonded.cpp"
+        carbon_file_1_src = carbon_spline_src_dir + carbon_file_1
+        carbon_file_2 = "reaxc_nonbonded.h"
+        carbon_file_2_src = carbon_spline_src_dir + carbon_file_2
+
+        # remove existing file
+        if (os.path.isfile(carbon_file_1)):
+            print("> Existing file " + carbon_file_1 + " removed ")
+            os.remove(carbon_file_1)
+        # copy modified file to src
+        shutil.copy(carbon_file_1_src, carbon_file_1)
+        print("> Copied updated file: " + carbon_file_1)
+
+        # remove existing file
+        if (os.path.isfile(carbon_file_2)):
+            print("> Existing file " + carbon_file_2 + " removed ")
+            os.remove(carbon_file_2)
+        # copy modified file to src
+        shutil.copy(carbon_file_2_src, carbon_file_2)
+        print("> Copied updated file: " + carbon_file_2)
+
+
+    print("> ")
+    print("> Setup of lammps source directory is complete.")
+    print("> Check the output above is as expected.")
+    print("> ")
+    print("> You can now compile lammps (using 4 cores):")
+    print("> make -j 4 kj_mpi  ")
+    print("> or build the shared library with:")
+    print("> make -j 4 kj_mpi mode=shlib")
+
+
+        
+
+
+
+# If we are running this script interactively, call the function safely
+if __name__ == '__main__':
+    use_voro=False
+    use_modified_reaxff=False
+    
+    print("  +------------------------------------------+")
+    print("  |        Lammps setup compile script       |")
+    print("  |                                          |")
+    print("  |               Kenny Jolley               |")
+    print("  |                June 2019                 |")
+    print("  |          kenny.jolley@gmail.com          |")
+    print("  +------------------------------------------+")
+    print("   ")
+
+    print("> This script sets up the lammps source directory ready for compiling.\n")
+
+    print("> You must clone the github repo yourself with:")
+    print("   git clone https://github.com/lammps/lammps.git mylammps")
+    print("> Or:")
+    print("   git clone git://github.com/lammps/lammps.git mylammps\n")
+
+    print(">  This script should then be run from the src directory of your lammps installation")
+    print(">  The current directory is: ")
+    print(os.getcwd())
+    
+    # Ask the user if they want to continue
+    if sys.version_info[0] < 3:
+        user_choice = str(raw_input('Do you wish to continue? (y/n) : ')).lower().strip()
+    else:
+        user_choice = str(input('Do you wish to continue? (y/n) : ')).lower().strip()
+    if ((user_choice != 'yes') and (user_choice != 'y') ):
+        sys.exit()
+
+    # Ask user if VORO++ should be included
+    print("   ")
+    print("> Lammps can be built with the VORO++ package   ")
+    print("> To use this package, you must install it before compiling lammps, see instructions in lib/voronoi")
+    print("> VORO++ is available here:   ")
+    print("> http://math.lbl.gov/voro++/ ")
+
+    if sys.version_info[0] < 3:
+        user_choice = str(raw_input('Do you wish to include VORO++ ? (y/n) : ')).lower().strip()
+    else:
+        user_choice = str(input('Do you wish to include VORO++ ? (y/n) : ')).lower().strip()
+    if ((user_choice == 'yes') or (user_choice == 'y') ):
+        use_voro = True
+
+
+
+    print("   ")
+    print("> A custom splining function between ReaxFF and ZBL for carbon systems can be added.")
+    print("> This is available using the customised ReaxFF code.")
+    if sys.version_info[0] < 3:
+        user_choice = raw_input('Do you wish to use the custom ReaxFF code? (y/n)?: ')
+    else:
+        user_choice = input('Do you wish to use the custom ReaxFF code? (y/n)?: ')
+    if ((user_choice == 'yes') or (user_choice == 'y') ):
+        use_modified_reaxff = True
+    
+    print("   ")
+    # call the lammps_setup_custom_compile function
+    lammps_setup_custom_compile(verbose=True, use_voro=use_voro, use_modified_reaxff=use_modified_reaxff)
